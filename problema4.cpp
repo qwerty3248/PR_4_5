ba298f5eff7cd2f69ef69373f34b84df92fdbcb3
qwerty3248 <jesuslosada4484@gmail.com>
2024-04-29 12:38:08 +0200

    Comienzo

---
+++ problema4.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main (void){
+    
+
+  int ini_x, ini_y;
+  cout << "Pos x para el inicio: ";
+  cin >> ini_x;
+  cout << endl << "Pos y para el inicio: ";
+  cin >> ini_y;
+  cout << "\n";
+
+  int fin_x,fin_y, tam_mat;
+
+  cout << "Introduce el tamanio de la matriz: ";
+  cin >> tam_mat;
+  
+  
+  
+
+
+
+
+
+}
+
